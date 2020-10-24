71fea704e526a20458679229b2b3d202ea60827c
KimJeongho <kimjeongho@archive-asia.co.jp>
2020-10-25 00:29:26 +0900

    Decorator Pattern Commit

--- src/decorator/topping/LettuceDecorator.java
+++ src/decorator/topping/LettuceDecorator.java
@@ -5,11 +5,12 @@ import decorator.sandwich.Sandwich;
 public class LettuceDecorator extends ToppingDecorator {
 
 	public LettuceDecorator(Sandwich sandwich) {
-		super(sandwich); // 받은 객체를 super()를 통해 부모 생성자에게 넘겨줌
+		// 받은 객체: Bread
+		super(sandwich); // 받은 객체를 부모 생성자에게 넘겨줌
 	}
 	
 	public void make() {
-		super.make(); // super를 통해 부모 클래스의 make()메소드를 호출
+		super.make(); // 부모 클래스의 make()메소드를 호출
 		addLettuce();
 	}
 	
--- src/decorator/topping/PickleDecorator.java
+++ src/decorator/topping/PickleDecorator.java
@@ -5,11 +5,12 @@ import decorator.sandwich.Sandwich;
 public class PickleDecorator extends ToppingDecorator {
 
 	public PickleDecorator(Sandwich sandwich) {
+		// 받은 객체: LettuceDecorator
 		super(sandwich);
 	}
 	
 	public void make() {
-		super.make();
+		super.make(); // 이 떄의 super.make()는 LettuceDecorator의 make()를 호출한 결과
 		addPickle();
 	}
 	
