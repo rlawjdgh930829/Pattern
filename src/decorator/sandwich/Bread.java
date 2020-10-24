54bb2715193c4301cac8e832f4c64f7ca3979a4f
KimJeongho <kimjeongho@archive-asia.co.jp>
2020-10-24 23:26:26 +0900

    Decorator Pattern Start

---
+++ src/decorator/sandwich/Bread.java
@@ -0,0 +1,10 @@
+package decorator.sandwich;
+
+public class Bread extends Sandwich {
+
+	@Override
+	public void make() {
+		 System.out.println("빵추가");
+	}
+
+}
---
+++ src/decorator/sandwich/Sandwich.java
@@ -0,0 +1,7 @@
+package decorator.sandwich;
+
+public abstract class Sandwich {
+	
+	public abstract void make();
+	
+}
